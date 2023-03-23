1b3068e025ce930c7e462d55c9ae9e6448799f11
husseinTalal2 <hossientalal2@gmail.com>
2023-03-23 17:50:44 +0300

    using token from the env

--- src/Bot.ts
+++ src/Bot.ts
@@ -1,10 +1,11 @@
 import { Client } from "discord.js";
 import interactionCreate from "./listeneres/interactionCreate";
 import ready from "./listeneres/ready";
+import dotenv from "dotenv";
+dotenv.config();
 
 console.log("Bot is starting...");
-const token =
-  "MTA4ODIwNjc0NTgzMjI3MTkxNA.GwAR6D.s2d2VggUeLYK2sUM4MmcPuK6XqwYB-IUBwHMGc";
+const token = process.env.TOKEN;
 const client = new Client({
   intents: [],
 });
